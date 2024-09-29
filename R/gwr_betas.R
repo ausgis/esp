cd2410e6e28dd4deacff5e0f5fea714b57305baa
SpatLyu <lyu.geosocial@gmail.com>
2024-09-29 19:20:24 +0800

    update gwr_betas

--- R/gwr_betas.R
+++ R/gwr_betas.R
@@ -1,7 +1,34 @@
+#' Title
+#'
+#' @param formula
+#' @param data
+#' @param adaptive
+#' @param kernel
+#' @param multiscale
+#'
+#' @return A `tibble`
+#' @export
+#'
+#' @examples
+#' depression = system.file('extdata/Depression.csv',package = 'gdverse') |>
+#'   readr::read_csv() |>
+#'   sf::st_as_sf(coords = c('X','Y'), crs = 4326)
+#' gwr_betas(Depression_prevelence ~ ., data = depression)
+#' gwr_betas(Depression_prevelence ~ ., data = depression, multiscale = TRUE)
+#'
 gwr_betas = \(formula, data,
               adaptive = TRUE,
               kernel = "gaussian",
               multiscale = FALSE){
+  formula = stats::as.formula(formula)
+  formula.vars = all.vars(formula)
+  if (formula.vars[2] != "."){
+    data = dplyr::select(data,dplyr::all_of(formula.vars))
+  }
+  yname = formula.vars[1]
+  gname = sdsfun::sf_geometry_name(data)
+  xname = colnames(data)[-which(colnames(data) %in% c(yname,gname))]
+
   longlat = dplyr::if_else(sf::st_is_longlat(data),TRUE,FALSE,FALSE)
   if (multiscale) {
     suppressWarnings({g = GWmodel3::gwr_multiscale(
@@ -15,5 +42,9 @@ gwr_betas = \(formula, data,
       formula, data, adaptive = adaptive, kernel = kernel
     )})
   }
-  return(coef(m))
+
+  betas = coef(g) %>%
+    tibble::as_tibble() %>%
+    dplyr::select(dplyr::all_of(xname))
+  return(betas)
 }
