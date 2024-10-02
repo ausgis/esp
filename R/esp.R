e5e1fedbbb477985764d6cc68f3690ccdba84c30
SpatLyu <lyu.geosocial@gmail.com>
2024-10-03 00:41:50 +0800

    update espm print s3 method

--- R/esp.R
+++ R/esp.R
@@ -479,9 +479,9 @@ esp = \(formula, data, listw = NULL, yzone = NULL, discvar = "all", discnum = 3:
 #' @noRd
 print.espm = \(x, ...) {
   cat("***           Enhanced Stratified Power     \n")
-  cat("\n --------- Global Power of Determinat : --------\n")
+  cat("\n ----------- Global Power of Determinat : ----------\n")
   print(x$factor)
-  cat("\n -------- Global Variable Interaction : --------\n")
+  cat("\n ---------- Global Variable Interaction : ----------\n")
   print(dplyr::select(x$interaction,1:2))
   cat("\n### The others can be accessed through specific subsets of the epsm object.\n")
 }
