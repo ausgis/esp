d22f97aae2ab6bc81fd49577294d5068b0e78df0
SpatLyu <lyu.geosocial@gmail.com>
2024-11-08 15:15:47 +0800

    make RCMD check clean

--- R/globals.R
+++ R/globals.R
@@ -1,1 +1,3 @@
-utils::globalVariables(c("disc","rowid","simres","DiscNum","Variable"))
+utils::globalVariables(c("disc","rowid","simres","DiscNum","Variable",
+                         "Interaction", "Qv12", "Qvalue", "Qvtext",
+                         "Variable1", "Variable2", "Variable_col", "n"))
