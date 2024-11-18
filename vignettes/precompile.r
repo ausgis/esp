90e1b2e6690c9ad987fc087cde458054a08d55db
SpatLyu <lyu.geosocial@gmail.com>
2024-11-18 15:04:30 +0800

    update pdrm vignette

--- vignettes/precompile.r
+++ vignettes/precompile.r
@@ -1,7 +1,12 @@
 devtools::load_all()
 
-knitr::knit("vignettes/sesp.rmd.orig",
-            "vignettes/sesp.rmd")
+knitr::knit("vignettes/pdrm.rmd.orig",
+            "vignettes/pdrm.rmd")
 
 knitr::knit("vignettes/whysed.rmd.orig",
             "vignettes/whysed.rmd")
+
+knitr::knit("vignettes/sesp.rmd.orig",
+            "vignettes/sesp.rmd")
+
+
